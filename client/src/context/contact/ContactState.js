65bc35cfec60b087e084956e5d261fd362df2724
Dominique Maack <dominiquemaack@gmail.com>
2020-04-07 10:59:00 -0600

    update and delete contacts with hardcoded data

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -36,7 +36,8 @@ const ContactState = props => {
                 phone: '333-333-3333',
                 type: 'professional'
             }
-        ]
+        ],
+        current: null
     }
 
     const [ state, dispatch ] = useReducer(contactReducer, initialState)
@@ -44,14 +45,56 @@ const ContactState = props => {
     // Actions:
 
     // Add Contact
+        // contact comes in (passed in) and we send it to the payload
+    const addContact = contact => {
+        contact.id = uuidv4()
+
+        // dispatch to reducer
+        dispatch({
+            type: ADD_CONTACT,
+            payload: contact
+        })
+    }
 
     // Delete Contact
+    const deleteContact = id => {
+
+        // dispatch to reducer
+        dispatch({
+            type: DELETE_CONTACT,
+            payload: id
+        })
+    }
 
     // Set Current Contact
+    const setCurrent = contact => {
+
+        // dispatch to reducer
+        dispatch({
+            type: SET_CURRENT,
+            payload: contact
+        })
+    }
+
 
     // Clear Current Contact
+    const clearCurrent = () => {
+
+        // dispatch to reducer
+        dispatch({
+            type: CLEAR_CURRENT
+        })
+    }
 
     // Update Contact
+    const updateContact = contact => {
+
+        // dispatch to reducer
+        dispatch({
+            type: UPDATE_CONTACT,
+            payload: contact
+        })
+    }
 
     // Filter Contacts
 
@@ -62,7 +105,13 @@ const ContactState = props => {
         <ContactContext.Provider
             //  anything we want to access from other components, including state and actions
             value = {{
-                contacts: state.contacts
+                contacts: state.contacts,
+                current: state.current,
+                addContact,
+                deleteContact,
+                setCurrent,
+                clearCurrent,
+                updateContact
             }} >
                 { props.children }
         </ContactContext.Provider>
