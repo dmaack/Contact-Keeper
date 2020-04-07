cc39a174778b8ba0d7b461610997dff7d877516b
Dominique Maack <dominiquemaack@gmail.com>
2020-04-07 13:42:54 -0600

    added auth components (register and login forms) and intial context api set up for auth

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,30 @@ import Home from './components/layout/Home'
 import About from './components/layout/About'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home}></Route>
-              <Route exact path='/about' component={About}></Route>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home}></Route>
+                <Route exact path='/about' component={About}></Route>
+                <Route exact path='/register' component={Register}></Route>
+                <Route exact path ='/login' component={Login}></Route>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -13,6 +13,8 @@ const NavBar = ({ title, icon }) => {
                 <li>
                     <Link to='/'>Home</Link>
                     <Link to='/about'>About</Link>
+                    <Link to='/register'>Register</Link>
+                    <Link to='/login'>Login</Link>
                 </li>
             </ul>
             
